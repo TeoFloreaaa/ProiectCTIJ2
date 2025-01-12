3d67510ccfac3cb39b071a92f574201bc6ac626a
TeoFloreaaa <teo_florea02@yahoo.com>
2025-01-12 15:14:46 +0200

    am adaugat sunete de fundal

---
+++ Assets/Scenes/BackgroundMusicController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class BackgroundMusicController : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        // Obține componenta AudioSource
+        audioSource = GetComponent<AudioSource>();
+
+        if (audioSource != null)
+        {
+            audioSource.loop = true; // Asigură-te că sunetul este setat să ruleze în buclă
+            audioSource.Play(); // Pornește sunetul de fundal
+        }
+        else
+        {
+            Debug.LogError("AudioSource nu este atașat la obiectul BackgroundMusic!");
+        }
+    }
+
+    public void SetVolume(float volume)
+    {
+        if (audioSource != null)
+        {
+            audioSource.volume = volume; // Setează volumul
+        }
+    }
+}
